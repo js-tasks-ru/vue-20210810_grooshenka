f7aaad6eac47d93f730e5f2713bcc7aa9ddb867b
Natalia Vasina <phonypony@yandex.com>
2021-08-17 16:46:41 +0300

    added CounterButtonApp

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,24 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+
+const Root = {
+  name: 'Root',
+
+  data() {
+    return {
+      count: 0,
+    };
+  },
+
+  methods: {
+    increment() {
+      this.count += 1;
+    },
+  },
+};
+
+const app = createApp(Root);
+const vm = app.mount('#app');
+
+window.vm = vm;
