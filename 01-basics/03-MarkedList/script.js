68be89630624f4961ea43579a39aa802fe0aa7f2
Natalia Vasina <phonypony@yandex.com>
2021-08-22 23:06:43 +0300

    MarkedList solution

--- 01-basics/03-MarkedList/script.js
+++ 01-basics/03-MarkedList/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // From https://jsonplaceholder.typicode.com/comments
 const emails = [
@@ -29,4 +29,52 @@ const emails = [
   'Isaias_Kuhic@jarrett.net',
 ];
 
-// Требуется создать Vue приложение
+const Root = {
+  name: 'Root',
+
+  data() {
+    return {
+      searchstring: '',
+      item: {
+        emailAddress: '',
+        matchesFilter: false,
+      },
+      filteredArray: [],
+    };
+  },
+
+  computed: {
+    filterEmails: function (e) {
+      if (this.searchstring.length > 0) {
+        for (let index = 0; index < this.filteredArray.length; index++) {
+          this.filteredArray[index].matchesFilter = this.filteredArray[index].emailAddress
+            .toLowerCase()
+            .includes(this.searchstring)
+            ? true
+            : false;
+        }
+      } else { //пустая строка запроса, обнулим маркеры
+        for (let index = 0; index < this.filteredArray.length; index++) {
+          this.filteredArray[index].matchesFilter = false;
+        }
+      }
+      return this.filteredArray;
+    },
+  },
+
+  beforeMount() {
+    //наполним filteredArray имейлами
+    for (let index = 0; index < emails.length; index++) {
+      let emailObject = {
+        emailAddress: emails[index],
+        matchesFilter: false,
+      };
+      this.filteredArray.push(emailObject);
+    }
+  },
+};
+
+const app = createApp(Root);
+const vm = app.mount('#app');
+
+window.vm = vm;
